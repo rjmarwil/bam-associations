45c7b7993e970718af902f63b3c49c15ac8141d4
Andrew Deitrick <ajdeitrick@gmail.com>
2015-03-16 08:04:55 -0600

    new spec for update

--- spec/features/appointment_management_spec.rb
+++ spec/features/appointment_management_spec.rb
@@ -51,4 +51,32 @@ feature "user appointments" do
     expect(page).to have_content "Upcoming Appointments for R2D2: 3"
     expect(page).to have_content "Doctors Assigned to R2D2: 2"
   end
+
+  scenario "can be edited" do
+    create_doctor(name: "Dr. Vader")
+    doctor = create_doctor(name: "Dr. Maul")
+    user = create_user(username: "R2D2", password: "beep boop bop")
+
+    Appointment.create!(user_id: user.id, doctor_id: doctor.id)
+
+    sign_in(user)
+
+    visit appointments_path
+
+    expect(page).to have_content "Upcoming Appointments for R2D2: 1"
+    expect(page).to have_content "Doctors Assigned to R2D2: 1"
+
+    click_on "Edit"
+
+    expect(page).to have_content "Appointment with Dr. Maul"
+
+    select("Dr. Vader", from: "Doctor")
+    fill_in "Date", with: "01/01/2530"
+
+    click_on "Update Appointment"
+
+    expect(page).to have_content "Appointment Updated with Dr. Vader"
+    expect(page).to have_content "Upcoming Appointments for R2D2: 1"
+    expect(page).to have_content "Doctors Assigned to R2D2: 1"
+  end
 end
