aaf25c50dda414662b5d68e77b9ebf0fe1135364
Jake Marwil <rjm02006@gmail.com>
2015-04-06 09:34:30 -0600

    add has many through relationships to each using appointments table

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,6 @@
 class Doctor < ActiveRecord::Base
   validates :name, presence: true
+  
+  has_many :appointments
+  has_many :users, through: :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true
+
+  has_many :appointments
+  has_many :doctors, through: :appointments
 end
